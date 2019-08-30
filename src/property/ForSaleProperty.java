bb15721196af171c3372813d9cef4dcfc33d19ab
61423 <chinguun9903@gmail.com>
2019-08-30 15:29:44 +1000

    added new classes

---
+++ src/property/ForSaleProperty.java
@@ -0,0 +1,7 @@
+package property;
+
+public class ForSaleProperty extends Property {
+    public ForSaleProperty(String address, int bedrooms, int bathrooms) {
+        super(address, bedrooms, bathrooms);
+    }
+}
